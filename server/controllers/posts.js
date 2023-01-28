abe82116c3a14a961683db25597632d819459853
Zendy <nguyenduc.051194@gmail.com>
2023-01-28 23:04:13 +0700

    Posts and post widget

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,11 +7,11 @@ export const createPost = async (req, res) => {
   try {
     const { userId, description, picturePath } = req.body;
     const user = await User.findById(userId);
-
     const newPost = new Post({
       userId,
       firstName: user.firstName,
       lastName: user.lastName,
+      location: user.location,
       description,
       userPicturePath: user.picturePath,
       picturePath,
